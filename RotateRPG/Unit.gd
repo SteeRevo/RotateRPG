6252e3573a3ed0170f5eaf91ea917fb18efd5d93
SteeRevo <69092521+SteeRevo@users.noreply.github.com>
2023-10-15 23:48:11 -0700

    added unit stats and turn order

--- RotateRPG/Unit.gd
+++ RotateRPG/Unit.gd
@@ -3,8 +3,20 @@ extends Node2D
 var player_
 var currentBattleGround
 
+@export var max_health = 1
+@export var current_health = 1
+@export var attack = 0
+@export var defense = 0
+@export var speed = 0
+
+
+
 func move_towards(target_pos):
 	var tween  = create_tween()
 	tween.tween_property(self, "position", target_pos, 1)
 
+func attack_unit(target_unit):
+	pass
 
+func get_speed():
+	return speed
--- RotateRPG/battle_scene.gd
+++ RotateRPG/battle_scene.gd
@@ -4,14 +4,49 @@ var turnManager = preload("res://TurnManager.tres")
 
 enum BATTLE_STATE {BASE, ATTACK_SELECT, SKILL_MENU, SKILL_SELECT, ROTATE_SELECT, IN_ATTACK, IN_SKILL, IN_ROTATE}
 
+@onready var battle_ui = $BattleUI
+
+var current_unit
+
+var unit_list = []
+var player_units = []
+var enemy_units = []
+
 
 func _ready():
-	$BattleGroundSet/BattleGround.set_current_unit($Player)
-	$BattleGroundSet/BattleGround2.set_current_unit($Player2)
-	$BattleGroundSet2/BattleGround.set_current_unit($Enemy)
+	$BattleGroundSet/BattleGround.set_current_unit($PlayerUnits/Player)
+	$BattleGroundSet/BattleGround2.set_current_unit($PlayerUnits/Player2)
+	$BattleGroundSet2/BattleGround.set_current_unit($EnemyUnits/Enemy)
+	
+	get_all_units()
+	
+	var max_speed = 0
+	print($PlayerUnits/Player.speed)
+	for unit in unit_list:
+		if unit.get_speed() > max_speed:
+			max_speed = unit.get_speed()
+			current_unit = unit
+	
+	print(current_unit)
+	
 	
 	turnManager.connect("player_turn_started", self._on_player_turn_started)
 	turnManager.connect("enemy_turn_started", self._on_enemy_turn_started)
+	
+
+	
+func get_all_units():
+
+	for unit in $PlayerUnits.get_children():
+		player_units.append(unit)
+		unit_list.append(unit)
+	for unit in $EnemyUnits.get_children():
+		enemy_units.append(unit)
+		unit_list.append(unit)
+
+	
+	print(unit_list)
+	
 
 func _on_player_turn_started():
 	print("player turn")
@@ -25,5 +60,8 @@ func _on_attack_pressed():
 
 
 func _on_rotate_pressed():
-	$Player.move_towards($BattleGroundSet/BattleGround2.global_position)
-	$Player2.move_towards($BattleGroundSet/BattleGround.global_position)
+	rotate_units(current_unit, $PlayerUnits/Player2)
+
+func rotate_units(unit1, unit2):
+	unit1.move_towards($BattleGroundSet/BattleGround2.global_position)
+	unit2.move_towards($BattleGroundSet/BattleGround.global_position)
